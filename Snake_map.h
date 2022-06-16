6966b5aa1dcdeec6cb7ed1b9d47fcbcd7fe19e9e
Minjae Lee <mmj9808@gmail.com>
2022-06-16 15:01:08 +0900

    Update Snake_map.h
    refactoring

--- Snake_map.h
+++ Snake_map.h
@@ -1,13 +1,24 @@
 #include <iostream>
-#include <ncurses.h>
-#define MAP_SIZE 21
 #ifndef __SNAKE_MAP__
 #define __SNAKE_MAP__
+#define MAP_SIZE 21
+#define GATE_SIZE 2
+        //     0        1         2        3           4           5            6         7
+enum color {WHITE, IMMUNE_WALL, WALL, SNAKE_HEAD, SNAKE_BODY, GROWTH_ITEM, POISON_ITEM, GATE};
 
-void map_init(int map[][MAP_SIZE]);
+extern int stage;
+extern bool waitGate;
+extern int forGate;
+extern int gate[][GATE_SIZE];
+extern int map[MAP_SIZE][MAP_SIZE];
+extern int map1[][GATE_SIZE];
+extern int map2[][GATE_SIZE];
+extern int map3[][GATE_SIZE];
+extern int map4[][GATE_SIZE];
+extern int walls[][GATE_SIZE];
 
-void draw_snakewindow(WINDOW* snake_win, int map[][MAP_SIZE]);
-
-void map_random_color(int map[][MAP_SIZE]);
+void map_init(int stage);
+void findImmuneWall();
+void makeGate();
 
 #endif
